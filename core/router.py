eabaec3c0f6235bc86c3c51783fe5126989bc23b
Shadow-V-Infinity <270384599+Shadow-V-Infinity@users.noreply.github.com>
2026-04-07 18:26:23 +0200

    Update router.py

--- core/router.py
+++ core/router.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from sports.nba.pages import live as nba_live
+from sports.nba.pages import avant_match as nba_avant_match
 
+def render_page(page: str):
+    if page == "🏀 NBA - Live":
+        nba_live.render()
+    elif page == "🏀 NBA - Avant-match":
+        nba_avant_match.render()
+    else:
+        st.write("Page non trouvée.")
