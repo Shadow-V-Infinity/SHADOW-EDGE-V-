f8f13b412e5409cb6d3d9bdf46cd53e82a3c6f5a
Shadow-V-Infinity <270384599+Shadow-V-Infinity@users.noreply.github.com>
2026-04-08 00:47:25 +0200

    Update player_profile_service.py

--- sports/nba/services/player_profile_service.py
+++ sports/nba/services/player_profile_service.py
@@ -7,7 +7,12 @@ from sports.nba.services.injury_service import InjuryService
 from sports.nba.services.matchup_service import MatchupService
 from sports.nba.services.playtype_service import PlayTypeService
 from sports.nba.services.props_service import PropsService
-from sports.nba.services.prediction_service import PredictionService
+
+from sports.nba.services.player_profile_utils import (
+    build_shotchart_heatmap,
+    build_playtype_radar,
+    get_player_clips,
+)
 
 from nba_api.stats.endpoints import commonplayerinfo, playergamelog
 
@@ -21,23 +26,21 @@ class PlayerProfileService:
         self.matchups = MatchupService()
         self.playtypes = PlayTypeService()
         self.props = PropsService()
-        self.predictions = PredictionService()
 
     # ---------------------------------------------------------------
-    # 1) Infos générales joueur
+    # Infos générales joueur
     # ---------------------------------------------------------------
     def get_player_info(self, player_id):
         try:
             info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
-            data = info["resultSets"][0]["rowSet"][0]
+            row = info["resultSets"][0]["rowSet"][0]
             headers = info["resultSets"][0]["headers"]
-
-            return {headers[i]: data[i] for i in range(len(headers))}
+            return {headers[i]: row[i] for i in range(len(headers))}
         except:
             return {}
 
     # ---------------------------------------------------------------
-    # 2) Derniers matchs
+    # Derniers matchs
     # ---------------------------------------------------------------
     def get_last_games(self, player_id, n=5):
         try:
@@ -60,15 +63,33 @@ class PlayerProfileService:
             return []
 
     # ---------------------------------------------------------------
-    # 3) Pack complet Player Profile
+    # Pack complet Player Profile
     # ---------------------------------------------------------------
-    def get_player_profile(self, player_id, team_id=None):
+    def get_player_profile(self, player_id):
+        shots = self.shotchart.get_player_shots(player_id, "2024-25")
+        playtypes = self.playtypes.get_player_playtypes(player_id, "2024-25")
+
         return {
             "info": self.get_player_info(player_id),
             "last_games": self.get_last_games(player_id),
-            "shotchart": self.shotchart.get_player_shots(player_id, "2024-25"),
-            "playtypes": self.playtypes.get_player_playtypes(player_id, "2024-25"),
+
+            # Shotchart
+            "shotchart_raw": shots,
+            "shotchart_heatmap": build_shotchart_heatmap(shots),
+
+            # Playtypes
+            "playtypes_raw": playtypes,
+            "playtypes_radar": build_playtype_radar(playtypes),
+
+            # Tracking
             "tracking": self.tracking.get_player_tracking(player_id, "2024-25"),
+
+            # Props
             "props": self.props.get_props_by_player(player_id),
+
+            # Injury
             "injury": self.injuries.get_player_injury(player_id),
+
+            # Clips vidéo
+            "clips": get_player_clips(self.pbp, player_id),
         }
