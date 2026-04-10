f625a8e58b6dcee37ae16dabbfbb4a60ab2ce5f8
Shadow-V-Infinity <270384599+Shadow-V-Infinity@users.noreply.github.com>
2026-04-10 10:37:58 +0200

    Create player.py

---
+++ app/services/delta_model/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, id: str, name: str, position: str = None, team_id: str = None):
+        self.id = id
+        self.name = name
+        self.position = position
+       _id
